97a6d5f51ff99adc7e5913ec9f9a4f0dd0a43149
Daniella Taylor <daniella.taylor92@gmail.com>
2020-09-07 20:48:56 +0100

    refactor: remove basic css, include gulp + sass

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const nodemon = require('gulp-nodemon');
+
+gulp.task('server', (cb) => {
+    let started = false;
+    return nodemon({
+        script: 'start.js',
+        ext: 'js, pug',
+        ignore: ['node_modules/', 'public/']
+    }).on('start', () => {
+        if(!started) {
+            cb();
+            started = true;
+        }
+    })
+});
+
+gulp.task('styles', (cb) => {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css')), cb();
+});
+
+gulp.task('watch', (cb) => {
+    return gulp.watch('sass/**/*.scss', gulp.series('styles')), cb();
+});
+
+gulp.task('clean', (cb) => {
+    return del([
+        'css/main.css',
+    ]), cb();
+});
+
+gulp.task('default', gulp.parallel('clean', 'watch', 'server'));
